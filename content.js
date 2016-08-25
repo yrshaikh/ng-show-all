3f2b74a7af110b8bde4e4cb9bb8005fb0b282f43
Yasser shaikh <yasser.s@directi.com>
2016-08-25 12:30:58 +0530

    missed out commits.

--- content.js
+++ content.js
@@ -1,12 +1,2 @@
-/*for(var i = 0; i < $("[ng-show]").length-1; i++){
-	var oldAttrValue = $("[ng-show]").attr("ng-show");
-	oldAttrValue = "true || " + oldAttrValue;
-	$("[ng-show]").attr("ng-show", oldAttrValue);
-}
-
-for(var i = 0; i < $("[ng-hide]").length-1; i++){
-	var oldAttrValue = $("[ng-hide]").attr("ng-hide");
-	oldAttrValue = "false || " + oldAttrValue;
-	$("[ng-hide]").attr("ng-hide", oldAttrValue);
-}*/
-$("<style type='text/css'> .ng-hide{display: block !important;} </style>").appendTo("head");
+//$("<style type='text/css'> .{display: block !important;} </style>").appendTo("head");
+$(".ng-hide").removeClass("ng-hide")
